585ff9f92811259d0456de49984ed49a83fab641
Arieh Norton <arieh.norton@gmail.com>
2022-06-08 19:48:18 +0300

    made paddle object

---
+++ paddle_right.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class PaddleRight(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.create_paddle()
+
+    def create_paddle(self):
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(350, 0)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(350, new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(350, new_y)
