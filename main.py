7b694638f62e96f75685d2c244278a64b6f3e0c6
Arieh Norton <arieh.norton@gmail.com>
2022-06-08 20:19:15 +0300

    fixed onkey problem

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
 
-
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(800, 600)
@@ -12,14 +11,13 @@ paddle_r = Paddle("right")
 paddle_l = Paddle("left")
 
 screen.listen()
-screen.onkey(paddle_r.go_up(), "Up")
-screen.onkey(paddle_r.go_down(), "Down")
-screen.onkey(paddle_l.go_up(), "w")
-screen.onkey(paddle_l.go_down(), "s")
+screen.onkey(paddle_r.go_up, "Up")
+screen.onkey(paddle_r.go_down, "Down")
+screen.onkey(paddle_l.go_up, "w")
+screen.onkey(paddle_l.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
 
 screen.exitonclick()
