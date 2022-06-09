2962bf873275b1891a3deb7dfb5c1d97228f50e6
Arieh Norton <arieh.norton@gmail.com>
2022-06-09 10:29:30 +0300

    added ball to main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,6 +12,8 @@ screen.tracer(0)
 paddle_r = Paddle("right")
 paddle_l = Paddle("left")
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(paddle_r.go_up, "Up")
 screen.onkey(paddle_r.go_down, "Down")
@@ -17,7 +21,14 @@ screen.onkey(paddle_l.go_up, "w")
 screen.onkey(paddle_l.go_down, "s")
 
 game_is_on = True
+speed = 0.4
 while game_is_on:
+    time.sleep(speed)
     screen.update()
+    ball.move()
+
+    if ball.xcor() == 280 or ball.xcor() == -280:
+        ball.bounce()
+
 
 screen.exitonclick()
