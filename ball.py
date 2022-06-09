9d52548a6c9a446524dd7db198cdeb2b86a3016b
Arieh Norton <arieh.norton@gmail.com>
2022-06-09 10:29:16 +0300

    created ball object and moving methods

--- ball.py
+++ ball.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 
 class Ball(Turtle):
@@ -6,5 +7,39 @@ class Ball(Turtle):
         super().__init__()
         self.penup()
         self.shape("circle")
-        self.shapesize(20, 20)
+        self.color("white")
         self.goto(0, 0)
+        directions = ["up_right", "down_right", "up_left", "down_left"]
+        self.direction = random.choice(directions)
+
+    def move(self):
+        if self.direction is "up_right":
+            new_x = self.xcor() + 10
+            new_y = self.ycor() + 10
+            self.goto(new_x, new_y)
+        elif self.direction is "down_right":
+            new_x = self.xcor() + 10
+            new_y = self.ycor() - 10
+            self.goto(new_x, new_y)
+        elif self.direction is "up_left":
+            new_x = self.xcor() - 10
+            new_y = self.ycor() + 10
+            self.goto(new_x, new_y)
+        elif self.direction is "down_left":
+            new_x = self.xcor() - 10
+            new_y = self.ycor() - 10
+            self.goto(new_x, new_y)
+
+    def bounce(self):
+        if self.direction is "up_right":
+            self.direction = "down_right"
+            self.move()
+        elif self.direction is "down_right":
+            self.direction = "up_right"
+            self.move()
+        elif self.direction is "up_left":
+            self.direction = "down_left"
+            self.move()
+        elif self.direction is "down_left":
+            self.direction = "up_left"
+            self.move()
